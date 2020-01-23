bae28fbf4c95bcfca0b81f46763cf10cbb8c291d
olahrmann <oliver.lahrmann@students.berufsakademie-oldenburg.de>
2020-01-23 15:30:41 +0100

    First

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $('#dtBasicExample').DataTable({});
+    $('.dataTables_length').addClass('bs-select');
+    });
+
+window.onload = function(){
+        $.get("sidebar.html", function(data){
+            $("#include").html(data);
+        })
+    }
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+        /* Make an HTTP request using the attribute value as the file name: */
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+            if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+            if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+            /* Remove the attribute, and call this function once more: */
+            elmnt.removeAttribute("w3-include-html");
+            includeHTML();
+            }
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /* Exit the function: */
+        return;
+        }
+    }
+    }
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+function generateValue(){
+
+      'apple'.split('').forEach(function (c) {
+        console.log(c + ': ' + c.charCodeAt(0));
+      });}
---
+++ run.py
@@ -0,0 +1,34 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+from csv import DictReader
+from itertools import groupby
+from operator import itemgetter
+from io import StringIO
+import csv
+
+root = os.path.dirname(os.path.abspath(__file__))
+templates_dir = os.path.join(root, 'templates')
+env = Environment( loader = FileSystemLoader(templates_dir) )
+table = env.get_template('table.html')
+details = env.get_template('singlePageTemplate.html')
+#TODO generator for single rows out of the csv file
+#dircetly connected via links
+
+filename = os.path.join(root, 'html', 'test.html')
+
+
+
+
+rdr= csv.reader( open("csv/Coding.csv", "r" ), delimiter=';' )
+csv_data = [ row for row in rdr ]
+i=1
+for row in csv_data:
+
+    i = i+1
+    singlepage = os.path.join(root, 'html', row[1]+'.html')
+    with open(singlepage, 'w+') as f:
+        f.write(details.render(data=row))
+
+with open(filename, 'w+') as f:
+    f.write(table.render(data=csv_data))
+

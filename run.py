5a3707dd34575f20e435ffa486ad36a1ec9f1758
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2020-12-30 21:57:45 +0100

    about and imprint and footer

--- run.py
+++ run.py
@@ -23,12 +23,10 @@ def build_templates():
   filename_consultancies = os.path.join(root, 'html', 'consultancies.html')
   filename_academics = os.path.join(root, 'html', 'academics.html')
   about = os.path.join(root, 'html', 'about.html')
-  helpPage = os.path.join(root, 'html', 'help.html')
   vendors_folder = os.path.join(root,"html", "vendors")
 
   # Load content
   aboutContent = open("templates/about.html","r").read()
-  helpContent = open("templates/help.html","r").read()
   vendors_csv = csv.reader( open("csv/Vendors_Coding.csv", "r" ), delimiter=';' )
   # consultancies_csv = csv.reader(open("csv/Consultancies_Coding.csv", "r"), delimiter=';')
   # academics_csv = csv.reader(open("csv/Academics_Coding.csv", "r"), delimiter=";")
@@ -44,9 +42,6 @@ def build_templates():
   # run the templating engine
   with open(about, 'w+') as f:
       f.write(contentPage.render(data=aboutContent))
-
-  with open(helpPage, 'w+') as f:
-      f.write(contentPage.render(data=helpContent))
 
   for row in vendors_data[2:]:
       singlepage = os.path.join(vendors_folder, row[1]+'.html')
